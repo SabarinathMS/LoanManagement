e928de27c0c3eede424b225d250f4f24a9138d6f
sabarinath <sabarinathsri@gmail.com>
2020-08-28 08:04:56 +0530

    Spring Rest Application

---
+++ LMA-api/src/main/java/com/app/LMA/LmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.LMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmaApiApplication.class, args);
+	}
+
+}
---
+++ LMA-api/src/test/java/com/app/LMA/LmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.LMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
