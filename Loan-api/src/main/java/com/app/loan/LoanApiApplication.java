4a51b45fd0ccc3429c4b3165fb9328d94ffed0c0
sabarinath <sabarinathsri@gmail.com>
2020-08-31 18:40:17 +0530

    Microservice integrated

--- Loan-api/src/main/java/com/app/loan/LoanApiApplication.java
+++ Loan-api/src/main/java/com/app/loan/LoanApiApplication.java
@@ -2,9 +2,10 @@ package com.app.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class LoanApiApplication {
 
 	public static void main(String[] args) {
@@ -12,3 +13,4 @@ public class LoanApiApplication {
 	}
 
 }
+
